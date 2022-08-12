65235a5354148f383dd21ff9df526f168a10cb79
Jean Hwang <106926262+jeancwhwang@users.noreply.github.com>
2022-08-11 20:33:55 -0500

    Update cyclistic_data_exploration.sql

--- cyclistic_data_exploration.sql
+++ cyclistic_data_exploration.sql
@@ -1,4 +1,4 @@
--- Create table for SQLite
+-- Create table Schema to import data in SQLite
 
 CREATE TABLE "cyclistic" (
 	"ride_id"	TEXT,
@@ -22,27 +22,29 @@ SELECT COUNT(*)
 FROM cyclistic;
 
 -- Check ride_id length --  All length(ride_id) = 16
+
 SELECT length(ride_id),
-	   COUNT(*)
+       COUNT(*)
 FROM cyclistic
 GROUP BY 1;
 
--- ALL ride_id is unique
-SELECT
-	  ride_id,
-	 COUNT(*)
+-- All ride_id is unique
+
+SELECT	ride_id,
+	COUNT(*)
 FROM cyclistic
 GROUP BY 1
 HAVING COUNT(*)> 0;
 
 -- No duplicates
+
 WITH Row_num_cte AS (
 SELECT *,
-		ROW_NUMBER() OVER(PARTITION BY ride_id,
-									rideable_type,
-									started_at,
-									ended_at
-						   ORDER BY ride_id) AS RowNum
+       ROW_NUMBER() OVER(PARTITION BY ride_id,
+				      rideable_type,
+				      started_at,
+				      ended_at
+				      ORDER BY ride_id) AS RowNum
 FROM cyclistic
 )
 SELECT RowNum
@@ -51,8 +53,8 @@ WHERE RowNum > 1;
 
 
 -- 12 reversed data for started_at & ended_at
-SELECT
-		julianday(ended_at)-julianday(started_at) as julianday_diff
+
+SELECT julianday(ended_at)-julianday(started_at) as julianday_diff
 FROM cyclistic
 WHERE julianday(ended_at)-julianday(started_at) < 0;
 
@@ -61,27 +63,24 @@ WHERE julianday(ended_at)-julianday(started_at) < 0;
 
 UPDATE cyclistic
 SET	started_at = replace(started_at,started_at,ended_at),
-	  ended_at = replace(ended_at,ended_at,started_at)
+	ended_at = replace(ended_at,ended_at,started_at)
 WHERE started_at > ended_at;
 
 -- FLAG: 54 people have the same started_at and ended_at. Might need to delete from the dataset
-SELECT
-		distinct member_casual,
-		COUNT(julianday(ended_at)-julianday(started_at)) as julianday_diff
+
+SELECT  distinct member_casual,
+        COUNT(julianday(ended_at)-julianday(started_at)) as julianday_diff
 FROM cyclistic
 WHERE julianday(ended_at)-julianday(started_at) = 0
 GROUP BY 1;
 
-
-
 -- rideable_type: no nulls, 3 variables
 -- Get percentages of each rideable_type
 
-SELECT
-	  DISTINCT rideable_type,
-	  count(*),
-	  (select count(*) from cyclistic) as total,
-	  count(*)*100/ (select count(*) from cyclistic) as 'pct%'
+SELECT	 DISTINCT rideable_type,
+	 count(*),
+	 (select count(*) from cyclistic) as total,
+	 count(*)*100/ (select count(*) from cyclistic) as 'pct%'
 FROM cyclistic
 GROUP BY 1
 ORDER BY 4 DESC;
@@ -103,37 +102,37 @@ SELECT
 FROM cyclistic;
 
 -- Get counts of day_of_week
-SELECT
-		distinct day_of_week,
-		count(*)
+
+SELECT	distinct day_of_week,
+	count(*)
 FROM Ride_Time
 GROUP BY 1;
 
 -- Start_coordinates counts (most popular start location)
-SELECT
-		start_lat||' '|| start_lng AS start_coordinates,
-		COUNT(*),
-		(SELECT COUNT(*) FROM cyclistic) AS Total,
-		COUNT(*)*100/(SELECT COUNT(*) FROM cyclistic) AS Pct_start_coordinates
+
+SELECT	start_lat||' '|| start_lng AS start_coordinates,
+	COUNT(*),
+	(SELECT COUNT(*) FROM cyclistic) AS Total,
+	COUNT(*)*100/(SELECT COUNT(*) FROM cyclistic) AS Pct_start_coordinates
 FROM cyclistic
 GROUP BY 1
 ORDER BY 2 DESC;
 
 -- End_coordinates counts (most popular end location)
-SELECT
-		end_lat||' '|| end_lng AS end_coordinates,
-		COUNT(*),
-		(SELECT COUNT(*) FROM cyclistic) AS Total,
-		COUNT(*)*100/(SELECT COUNT(*) FROM cyclistic) AS Pct_start_coordinates
+
+SELECT	end_lat||' '|| end_lng AS end_coordinates,
+	COUNT(*),
+	(SELECT COUNT(*) FROM cyclistic) AS Total,
+	COUNT(*)*100/(SELECT COUNT(*) FROM cyclistic) AS Pct_start_coordinates
 FROM cyclistic
 GROUP BY 1
 ORDER BY 2 DESC;
 
-
 -- Create View for Coordinates
+
 CREATE VIEW Coordinates
 AS
 SELECT *,
-		start_lat||' '|| start_lng AS start_coordinates,
-		end_lat||' '|| end_lng AS end_coordinates
+       start_lat||' '|| start_lng AS start_coordinates,
+       end_lat||' '|| end_lng AS end_coordinates
 FROM cyclistic;
